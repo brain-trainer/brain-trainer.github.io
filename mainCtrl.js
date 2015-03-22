36e6d2dbb2f3865489e822d047661e210ed209b0
Michael Vietluzhskih <mishavetl@gmail.com>
2015-03-22 21:42:34 +0200

    deleted directive

--- mainCtrl.js
+++ mainCtrl.js
@@ -137,21 +137,3 @@ app.controller('myCtrl', function($scope) {
       $scope.genExp(parseInt($scope.from), parseInt($scope.to));
     }
 });
-
-app.directive('focusOn', function() {
-   return function(scope, elem, attr) {
-      scope.$on('focusOn', function(e, name) {
-        if(name === attr.focusOn) {
-          elem[0].focus();
-        }
-      });
-   };
-});
-
-app.factory('focus', function ($rootScope, $timeout) {
-  return function(name) {
-    $timeout(function (){
-      $rootScope.$broadcast('focusOn', name);
-    });
-  }
-});
